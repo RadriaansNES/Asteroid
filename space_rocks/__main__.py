a0141eeb10750805794eeaa886af5bdea18d0b83
Ryan Adriaans <Ryaan.1996s@hotmail.com>
2023-05-05 10:05:34 -0400

    Display incpororated.

---
+++ space_rocks/__main__.py
@@ -0,0 +1,5 @@
+from game import SpaceRocks
+## Creates new instance and starts
+if __name__ == "__main__":
+    space_rocks = SpaceRocks()
+    space_rocks.main_loop()
--- space_rocks/game.py
+++ space_rocks/game.py
@@ -1,26 +1,34 @@
 import pygame
+from utils import load_sprite
 
 class SpaceRocks:
+    ## Create class, initalize statements
     def __init__(self):
         self._init_pygame()
         self.screen = pygame.display.set_mode((800, 600))
+        self.background = load_sprite("space", False)
 
+    ## Reg game loop
     def main_loop(self):
         while True:
             self._handle_input()
             self._process_game_logic()
             self._draw()
 
+    ## Starts pygame
     def _init_pygame(self):
         pygame.init()
         pygame.display.set_caption("Space Rocks")
 
     def _handle_input(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (
+            event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                quit()
 
     def _process_game_logic(self):
         pass
-
+    ## Draw content to screen, here just a blank screen
     def _draw(self):
-        self.screen.fill((0, 0, 255))
+        self.screen.blit(self.background, (0, 0))
         pygame.display.flip()
